7442272815dc7a434e98eef7e6903ab43d6e525c
BlazeGDP <gaduque2003@gmail.com>
2023-02-08 17:52:01 -0500

    implement ejercicio 1

---
+++ ejercicios de repaso/ejercicio_1.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    nombre = input("ingrese su nombre: ")
+    edad = int(input("ingrese su edad: "))
+
+    print(f"hola {nombre}! increible, tienes {edad} años de edad. ")
