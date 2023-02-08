2ffc9318ab32c84657c6020f4309c5ff0d6144f5
BlazeGDP <gaduque2003@gmail.com>
2023-02-08 18:39:08 -0500

    implement ejercicio 4

---
+++ ejercicios de repaso/ejercicio_4.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    numero = int(input("digite un numero entero: "))
+    if numero % 2 == 0:
+        print("el numero", numero, "es par")
+    else:
+        print("el numero", numero, "es impar")
