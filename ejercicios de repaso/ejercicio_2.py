f50c5cce043e45af3b452e4f6911c9b4ff017279
BlazeGDP <gaduque2003@gmail.com>
2023-02-08 18:14:57 -0500

    implement ejercicio 2

---
+++ ejercicios de repaso/ejercicio_2.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    import math
+    p = math.pi
+    radio = float(input("digite el valor del radio: "))
+    AreaCirculo = p*(radio**2)
+    print(f"el area del circulo con el radio dado es: {AreaCirculo}")
