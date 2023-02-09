0d44e35418840094c2b742b616a49b0c6d826bbd
BlazeGDP <gaduque2003@gmail.com>
2023-02-08 22:13:13 -0500

    recreate ejercicio 9

--- ejercicios de repaso/ejercicio_9.py
+++ ejercicios de repaso/ejercicio_9.py
@@ -12,10 +12,3 @@ if __name__ == "__main__":
   matriz = matriz_numeros()
   for fila in matriz:
     print(fila)
-  def factorial(num):
-    fact = 1
-    for i in range(1,num+1):
-      fact*= i
-    return fact
-
-  factorial(5)
