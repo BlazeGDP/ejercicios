1d9fc58ac935bd6011741708886b025e111d24eb
BlazeGDP <gaduque2003@gmail.com>
2023-02-08 22:58:29 -0500

    implement ejercicio 13

--- ejercicios de repaso/ejercicio_13.py
+++ ejercicios de repaso/ejercicio_13.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     a = int(input("Ingrese primer numero: \n"))
     b = int(input("Ingrese segundo numero: \n"))
     return f"La suma es {a+b}, la multiplicacion es {a*b}, la resta es {a-b} y la division es {a/b}"
-  operaciones()#Solo enteros
+print(f"Las operaciones numeros ingresados son: ", operaciones())
