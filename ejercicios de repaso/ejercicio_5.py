c628c9fbd615ea38d8da4f1bc66b1e8d7ae4f0c9
BlazeGDP <gaduque2003@gmail.com>
2023-02-08 19:25:07 -0500

    implement ejercicio 5

---
+++ ejercicios de repaso/ejercicio_5.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    fahrenheit = float(input("ingrese los grados fahrenheit a convertir: "))
+    celsius = (fahrenheit - 32)*5/9
+    print(f"{fahrenheit}°F a celsius es:", celsius, "°C")
