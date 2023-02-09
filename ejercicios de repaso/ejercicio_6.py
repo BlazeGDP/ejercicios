b81372eb8433579c2669f079fb9b9cfadcb298ea
BlazeGDP <gaduque2003@gmail.com>
2023-02-08 20:59:20 -0500

    implement ejercicio 6

---
+++ ejercicios de repaso/ejercicio_6.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    numerosLista = [1, 6, 25, 4, 45, 69, 21, 19, 20, 5]
+    suma = 0
+    for i in numerosLista:
+        suma = suma + i
+    print("la suma de los numeros es:", suma)
